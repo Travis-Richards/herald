711c9e8be9975583942d4905e490aa763977912b
Taylor Holberton <taylorcholberton@gmail.com>
2020-02-11 22:22:24 -0500

    Using QMake for Travis CI build

---
+++ scripts/TravisBuild.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$TRAVIS" == "" ]; then
+  echo "This script should only be run on Travis CI builds"
+  exit 1
+fi
+
+set -e
+set -u
+
+mkdir build && cd build
+
+qmake=qmake
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  qmake=/usr/local/opt/qt/bin/qmake
+fi
+
+$qmake -config release ..
+
+make
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  /usr/local/opt/qt/bin/macdeployqt herald.app -dmg
+fi
