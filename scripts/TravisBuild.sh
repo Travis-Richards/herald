a8f3026e111c095e2f80578ba93d679faa2250fd
Taylor Holberton <taylorcholberton@gmail.com>
2020-02-18 23:38:02 -0500

    Adjusted macOS deployment script

--- scripts/TravisBuild.sh
+++ scripts/TravisBuild.sh
@@ -7,20 +7,33 @@ fi
 
 set -e
 
+# A working space for all generated files.
 mkdir build && cd build
 
+# Where the local install will go.
+local_instdir="$PWD/herald"
+
+# Make sure we get full optimizations
+# for deployment builds
 cmake_opts="-DCMAKE_BUILD_TYPE=Release"
 
+# Ensure CMake installs the project into a location
+# within the build directory.
+cmake_opts="$cmake_opts -DCMAKE_INSTALL_PREFIX=herald"
+
+# Special care has to be taken on macOS
+# due to the way brew installs packages.
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
   cmake_opts="$cmake_opts -DQt5_DIR=$(brew --prefix qt5)/lib/cmake/Qt5"
 fi
 
 cmake .. $cmake_opts
-
 make
+make install
 
+# Make sure we run deployment scripts for macOS builds.
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  /usr/local/opt/qt/bin/macdeployqt herald-hub.app -dmg
+  /usr/local/opt/qt/bin/macdeployqt herald.app -dmg
 fi
 
 # This is just for debugging
