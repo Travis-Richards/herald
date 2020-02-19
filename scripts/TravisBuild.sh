7c3f0fdbbb7511da31a3075e7f1c787ab1aad40d
Taylor Holberton <taylorcholberton@gmail.com>
2020-02-18 23:01:48 -0500

    Fixed Qt path

--- scripts/TravisBuild.sh
+++ scripts/TravisBuild.sh
@@ -10,9 +10,7 @@ set -e
 mkdir build && cd build
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  Qt5_DIR_OPT="-DQt5_DIR=/usr/local/opt/qt/5.13.2"
-  ls -la /usr/local/opt/qt/
-  ls -la /usr/local/opt/qt/5.13.1
+  Qt5_DIR_OPT="-DQt5_DIR=/usr/local/opt/qt/"
 fi
 
 cmake .. "-DCMAKE_BUILD_TYPE=Release" $Qt5_DIR_OPT
